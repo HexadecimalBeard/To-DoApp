51093c813b28a07c5812e25a11b40eee2b1185ac
Taha <ttoksoz41@gmail.com>
2018-09-05 17:21:22 +0300

    Edittext edit

--- app/src/main/java/com/exampleapp/to_do/MainActivity.java
+++ app/src/main/java/com/exampleapp/to_do/MainActivity.java
@@ -2,13 +2,18 @@ package com.exampleapp.to_do;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
