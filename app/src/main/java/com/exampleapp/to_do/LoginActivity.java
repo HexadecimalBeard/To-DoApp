c0c1c64c2b9b8ce7d78099a14214452f1caecbee
Melih <kokmelih@gmail.com>
2018-09-06 18:37:14 +0300

    Fundamental animations created

---
+++ app/src/main/java/com/exampleapp/to_do/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.exampleapp.to_do;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ImageView loginScreenIcon = ( ImageView ) findViewById(R.id.loginScreenIcon);
+
+        ImageView loginscreenblueupper_imageView = ( ImageView ) findViewById(R.id.loginscreenblue_imageview);
+
+        ImageView loginscreenwhitedown_imageView = ( ImageView ) findViewById(R.id.loginscreenwhite_imageview);
+
+        loginscreenblueupper_imageView.setY(-1000);
+
+        loginscreenwhitedown_imageView.setY(1000);
+
+        loginscreenblueupper_imageView.animate().translationYBy(1000).setDuration(675);
+
+        loginscreenwhitedown_imageView.animate().translationYBy(-1000).setDuration(675);
+
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        loginScreenIcon.startAnimation(startAnimation);
+
+    }
+
+
+}
--- app/src/main/java/com/exampleapp/to_do/MainActivity.java
+++ app/src/main/java/com/exampleapp/to_do/MainActivity.java
@@ -1,19 +1,32 @@
 package com.exampleapp.to_do;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button=findViewById(R.id.buttonpass);
 
+    }
+
+    public void loginPass(View view){
+
+        Intent intent= new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
 
     }
 }
