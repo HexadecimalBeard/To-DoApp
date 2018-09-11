b2cf8ef8e3afc8abfd042043c6eaefe95fdb4d3a
Melih <kokmelih@gmail.com>
2018-09-11 20:06:05 +0300

    Login and Register page design made.

--- app/src/main/java/com/exampleapp/to_do/LoginActivity.java
+++ app/src/main/java/com/exampleapp/to_do/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.exampleapp.to_do;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -12,11 +14,23 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button btn= findViewById(R.id.activitylogin_login_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
